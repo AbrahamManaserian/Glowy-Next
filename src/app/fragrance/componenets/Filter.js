f0d618d6fd89a619c74681a0335ca2e851fc412e
AbrahamManaserian <abraham020788@gmail.com>
2025-09-22 15:10:59 +0400

    setup cleaned filter for fragrance page

--- src/app/fragrance/componenets/Filter.js
+++ src/app/fragrance/componenets/Filter.js
@@ -6,9 +6,7 @@ import {
   Collapse,
   FormControlLabel,
   FormGroup,
-  FormHelperText,
   Grid,
-  InputBase,
   TextField,
   Typography,
 } from '@mui/material';
@@ -16,325 +14,193 @@ import { useState } from 'react';
 import RemoveOutlinedIcon from '@mui/icons-material/RemoveOutlined';
 import AddOutlinedIcon from '@mui/icons-material/AddOutlined';
 
-export default function Filter({ handleChangeParams, paramsState, handleChangeCategoryParams, makeRout }) {
-  const [openCollapseGender, setOpenCollapseGender] = useState(true);
-  const [openCollapseCategory, setOpenCollapseCategory] = useState(true);
-  const [openCollapsePrice, setOpenCollapsePrice] = useState(true);
+const categoryObj = {
+  fragrance: 'Fragrance',
+  'car-air-fresheners': 'Car-Air-Fresheners',
+  'home-air-fresheners': 'Home Air Fresheners',
+  deodorant: 'Deodorant',
+};
+
+const genderBoj = { women: 'Women', men: 'Men', uni: 'Uni' };
+
+const textFieldStyle = {
+  width: '120px',
+  '& .MuiOutlinedInput-root': {
+    height: '40px',
+    fontSize: '14px',
+    backgroundColor: '#d2cccc4d',
+    borderRadius: '8px',
+    padding: '0 20px',
+    '& fieldset': {
+      border: '1px solid #ffffffff',
+    },
+    '&.Mui-focused fieldset': {
+      border: '1px solid #030303dd',
+    },
+    '& input[type=number]': {
+      MozAppearance: 'textfield', // Firefox
+    },
+    '& input[type=number]::-webkit-outer-spin-button, & input[type=number]::-webkit-inner-spin-button': {
+      WebkitAppearance: 'none', // Chrome, Safari, Edge
+      margin: 0,
+    },
+  },
+  '& .MuiOutlinedInput-input': {
+    padding: 0, // remove default padding
+  },
+};
+
+const ColllapseItem = ({ prop, name, open, handleCangeCollapse }) => {
+  return (
+    <Box sx={{ display: 'flex', justifyContent: 'space-between', my: '10px' }}>
+      <Typography sx={{ color: '#263045fb', fontWeight: 500 }}> {name} </Typography>
+      {open ? (
+        <RemoveOutlinedIcon
+          onClick={() => handleCangeCollapse(prop, false)}
+          sx={{ color: '#263045fb', cursor: 'pointer' }}
+        />
+      ) : (
+        <AddOutlinedIcon
+          onClick={() => handleCangeCollapse(prop, true)}
+          sx={{ color: '#263045fb', cursor: 'pointer' }}
+        />
+      )}
+    </Box>
+  );
+};
+
+const FormItem = ({ array, prop, value, name, makeRout, handleChangeArrayParams }) => {
+  return (
+    <FormControlLabel
+      sx={{
+        '& .MuiFormControlLabel-label': {
+          fontSize: '14px',
+          color: '#263045e9',
+        },
+      }}
+      control={
+        <Checkbox
+          size="small"
+          sx={{
+            borderRadius: '15px',
+            '&.Mui-checked': {
+              color: '#e54e36ff',
+            },
+          }}
+          checked={array.includes(value)}
+          onChange={() =>
+            makeRout ? handleChangeArrayParams(prop, value, true) : handleChangeArrayParams(prop, value)
+          }
+        />
+      }
+      label={name}
+    />
+  );
+};
+
+export default function Filter({ paramsState, handleChangeParams, makeRout, handleChangeArrayParams }) {
+  const [collapseItems, setCollapseItems] = useState({
+    gender: true,
+    category: true,
+    price: true,
+  });
+  //   console.log(makeRout);
+  const handleCangeCollapse = (key, value) => {
+    setCollapseItems({ ...collapseItems, [key]: value });
+  };
 
   return (
     <Grid container sx={{ width: '250px' }} direction={'column'}>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: '10px' }}>
-        <Typography sx={{ color: '#263045fb', fontWeight: 500 }}>Price</Typography>
-        {openCollapsePrice ? (
-          <RemoveOutlinedIcon
-            onClick={() => setOpenCollapsePrice(false)}
-            sx={{ color: '#263045fb', cursor: 'pointer' }}
-          />
-        ) : (
-          <AddOutlinedIcon
-            onClick={() => setOpenCollapsePrice(true)}
-            sx={{ color: '#263045fb', cursor: 'pointer' }}
-          />
-        )}
-      </Box>
-
-      <Collapse in={openCollapsePrice} timeout="auto" unmountOnExit>
+      <ColllapseItem
+        prop="price"
+        name="Price"
+        open={collapseItems.price}
+        handleCangeCollapse={handleCangeCollapse}
+      />
+      <Collapse in={collapseItems.price} timeout="auto" unmountOnExit>
         <Box sx={{ display: 'flex', justifyContent: 'space-between', my: '10px' }}>
           <TextField
             type="number"
             placeholder="Min price"
+            defaultValue={paramsState.minPrice}
+            onBlur={(e) =>
+              makeRout
+                ? handleChangeParams('minPrice', e.target.value, true)
+                : handleChangeParams('minPrice', e.target.value)
+            }
             variant="outlined"
             onKeyDown={(e) => {
               if (['e', 'E', '+', '-'].includes(e.key)) {
                 e.preventDefault();
               }
             }}
-            sx={{
-              width: '120px',
-              '& .MuiOutlinedInput-root': {
-                height: '40px',
-                fontSize: '14px',
-                backgroundColor: '#d2cccc4d',
-                borderRadius: '8px',
-                padding: '0 20px',
-                '& fieldset': {
-                  border: '1px solid #ffffffff',
-                },
-                '&.Mui-focused fieldset': {
-                  border: '1px solid #030303dd',
-                },
-                '& input[type=number]': {
-                  MozAppearance: 'textfield', // Firefox
-                },
-                '& input[type=number]::-webkit-outer-spin-button, & input[type=number]::-webkit-inner-spin-button':
-                  {
-                    WebkitAppearance: 'none', // Chrome, Safari, Edge
-                    margin: 0,
-                  },
-              },
-              '& .MuiOutlinedInput-input': {
-                padding: 0, // remove default padding
-              },
-            }}
+            sx={textFieldStyle}
             helperText="Min Price AMD"
           />
           <TextField
             type="number"
-            placeholder="Min price"
+            placeholder="Max price"
             variant="outlined"
             onKeyDown={(e) => {
               if (['e', 'E', '+', '-'].includes(e.key)) {
                 e.preventDefault();
               }
             }}
-            sx={{
-              width: '120px',
-              '& .MuiOutlinedInput-root': {
-                height: '40px',
-                fontSize: '14px',
-                backgroundColor: '#d2cccc4d',
-                borderRadius: '8px',
-                padding: '0 20px',
-                '& fieldset': {
-                  border: '1px solid #ffffffff',
-                },
-                '&.Mui-focused fieldset': {
-                  border: '1px solid #030303dd',
-                },
-                '& input[type=number]': {
-                  MozAppearance: 'textfield', // Firefox
-                },
-                '& input[type=number]::-webkit-outer-spin-button, & input[type=number]::-webkit-inner-spin-button':
-                  {
-                    WebkitAppearance: 'none', // Chrome, Safari, Edge
-                    margin: 0,
-                  },
-              },
-              '& .MuiOutlinedInput-input': {
-                padding: 0, // remove default padding
-              },
-            }}
+            defaultValue={paramsState.maxPrice}
+            onBlur={(e) =>
+              makeRout
+                ? handleChangeParams('maxPrice', e.target.value, true)
+                : handleChangeParams('maxPrice', e.target.value)
+            }
+            sx={textFieldStyle}
             helperText="Max Price AMD"
           />
         </Box>
       </Collapse>
-
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', my: '10px' }}>
-        <Typography sx={{ color: '#263045fb', fontWeight: 500 }}>Category</Typography>
-        {openCollapseCategory ? (
-          <RemoveOutlinedIcon
-            onClick={() => setOpenCollapseCategory(false)}
-            sx={{ color: '#263045fb', cursor: 'pointer' }}
-          />
-        ) : (
-          <AddOutlinedIcon
-            onClick={() => setOpenCollapseCategory(true)}
-            sx={{ color: '#263045fb', cursor: 'pointer' }}
-          />
-        )}
-      </Box>
-
-      <Collapse in={openCollapseCategory} timeout="auto" unmountOnExit>
+      <ColllapseItem
+        prop="category"
+        name="Category"
+        open={collapseItems.category}
+        handleCangeCollapse={handleCangeCollapse}
+      />
+      <Collapse in={collapseItems.category} timeout="auto" unmountOnExit>
         <FormGroup sx={{ pl: '8px', mb: '5px' }}>
-          <FormControlLabel
-            sx={{
-              '& .MuiFormControlLabel-label': {
-                fontSize: '14px',
-                color: '#263045e9',
-              },
-            }}
-            control={
-              <Checkbox
-                size="small"
-                sx={{
-                  borderRadius: '15px',
-                  '&.Mui-checked': {
-                    color: '#e54e36ff',
-                  },
-                }}
-                checked={paramsState.category.includes('fragrance')}
-                onChange={() =>
-                  makeRout
-                    ? handleChangeCategoryParams('fragrance')
-                    : handleChangeCategoryParams('fragrance', true)
-                }
+          {Object.keys(categoryObj).map((name, index) => {
+            return (
+              <FormItem
+                key={index}
+                handleChangeArrayParams={handleChangeArrayParams}
+                array={paramsState.category}
+                prop="category"
+                name={categoryObj[name]}
+                value={name}
+                makeRout={makeRout}
               />
-            }
-            label="Fragrance"
-          />
-          <FormControlLabel
-            sx={{
-              '& .MuiFormControlLabel-label': {
-                fontSize: '14px',
-                color: '#263045e9',
-              },
-            }}
-            control={
-              <Checkbox
-                size="small"
-                sx={{
-                  borderRadius: '15px',
-                  '&.Mui-checked': {
-                    color: '#e54e36ff',
-                  },
-                }}
-                checked={paramsState.category.includes('car-air-fresheners')}
-                onChange={() =>
-                  makeRout
-                    ? handleChangeCategoryParams('car-air-fresheners')
-                    : handleChangeCategoryParams('car-air-fresheners', true)
-                }
-              />
-            }
-            label="Car Air Fresheners"
-          />
-
-          <FormControlLabel
-            sx={{
-              '& .MuiFormControlLabel-label': {
-                fontSize: '14px',
-                color: '#263045e9',
-              },
-            }}
-            control={
-              <Checkbox
-                size="small"
-                sx={{
-                  borderRadius: '15px',
-                  '&.Mui-checked': {
-                    color: '#e54e36ff',
-                  },
-                }}
-                checked={paramsState.category.includes('home-air-fresheners')}
-                onChange={() =>
-                  makeRout
-                    ? handleChangeCategoryParams('home-air-fresheners')
-                    : handleChangeCategoryParams('home-air-fresheners', true)
-                }
-              />
-            }
-            label="Home Air Fresheners"
-          />
-          <FormControlLabel
-            sx={{
-              '& .MuiFormControlLabel-label': {
-                fontSize: '14px',
-                color: '#263045e9',
-              },
-            }}
-            control={
-              <Checkbox
-                size="small"
-                sx={{
-                  borderRadius: '15px',
-                  '&.Mui-checked': {
-                    color: '#e54e36ff',
-                  },
-                }}
-                checked={paramsState.category.includes('deodorant')}
-                onChange={() =>
-                  makeRout
-                    ? handleChangeCategoryParams('deodorant')
-                    : handleChangeCategoryParams('deodorant', true)
-                }
-              />
-            }
-            label="Deodorant"
-          />
+            );
+          })}
         </FormGroup>
       </Collapse>
-      <Box sx={{ display: 'flex', justifyContent: 'space-between', my: '10px' }}>
-        <Typography sx={{ color: '#263045fb', fontWeight: 500 }}>Gender</Typography>
-        {openCollapseGender ? (
-          <RemoveOutlinedIcon
-            onClick={() => setOpenCollapseGender(false)}
-            sx={{ color: '#263045fb', cursor: 'pointer' }}
-          />
-        ) : (
-          <AddOutlinedIcon
-            onClick={() => setOpenCollapseGender(true)}
-            sx={{ color: '#263045fb', cursor: 'pointer' }}
-          />
-        )}
-      </Box>
-
-      <Collapse in={openCollapseGender} timeout="auto" unmountOnExit>
+      <ColllapseItem
+        prop="gender"
+        name="Gender"
+        open={collapseItems.gender}
+        handleCangeCollapse={handleCangeCollapse}
+      />
+      <Collapse in={collapseItems.gender} timeout="auto" unmountOnExit>
         <FormGroup sx={{ pl: '8px', mb: '5px' }}>
-          <FormControlLabel
-            sx={{
-              '& .MuiFormControlLabel-label': {
-                fontSize: '14px',
-                color: '#263045e9',
-              },
-            }}
-            control={
-              <Checkbox
-                size="small"
-                sx={{
-                  borderRadius: '15px',
-                  '&.Mui-checked': {
-                    color: '#e54e36ff',
-                  },
-                }}
-                checked={paramsState.gender.includes('women')}
-                onChange={() =>
-                  makeRout
-                    ? handleChangeParams('gender', 'women')
-                    : handleChangeParams('gender', 'women', true)
-                }
+          {Object.keys(genderBoj).map((name, index) => {
+            return (
+              <FormItem
+                key={index}
+                handleChangeArrayParams={handleChangeArrayParams}
+                array={paramsState.gender}
+                prop="gender"
+                name={genderBoj[name]}
+                value={name}
+                makeRout={makeRout}
               />
-            }
-            label="Women"
-          />
-
-          <FormControlLabel
-            sx={{
-              '& .MuiFormControlLabel-label': {
-                fontSize: '14px',
-                color: '#263045e9',
-              },
-            }}
-            control={
-              <Checkbox
-                size="small"
-                sx={{
-                  borderRadius: '15px',
-                  '&.Mui-checked': {
-                    color: '#e54e36ff',
-                  },
-                }}
-                checked={paramsState.gender.includes('men')}
-                onChange={() =>
-                  makeRout ? handleChangeParams('gender', 'men') : handleChangeParams('gender', 'men', true)
-                }
-              />
-            }
-            label="Men"
-          />
-          <FormControlLabel
-            sx={{
-              '& .MuiFormControlLabel-label': {
-                fontSize: '14px',
-                color: '#263045e9',
-              },
-            }}
-            control={
-              <Checkbox
-                size="small"
-                sx={{
-                  borderRadius: '15px',
-                  '&.Mui-checked': {
-                    color: '#e54e36ff',
-                  },
-                }}
-                checked={paramsState.gender.includes('uni')}
-                onChange={() =>
-                  makeRout ? handleChangeParams('gender', 'uni') : handleChangeParams('gender', 'uni', true)
-                }
-              />
-            }
-            label="Uni"
-          />
+            );
+          })}
         </FormGroup>
       </Collapse>
     </Grid>
--- src/app/fragrance/page.js
+++ src/app/fragrance/page.js
@@ -1,39 +1,30 @@
 'use client';
 
-import {
-  Box,
-  Button,
-  Drawer,
-  FormControl,
-  Grid,
-  InputLabel,
-  MenuItem,
-  Select,
-  Typography,
-} from '@mui/material';
+import { Box, Button, Drawer, Grid, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
-import { usePathname, useSearchParams, useRouter } from 'next/navigation';
-import SortView from './componenets/SortView';
 import Filter from './componenets/Filter';
-import TuneIcon from '@mui/icons-material/Tune';
+import { useRouter, useSearchParams } from 'next/navigation';
+import SortView from './componenets/SortView';
 import CloseIcon from '@mui/icons-material/Close';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 export default function FragrancePage() {
-  const [openDrawer, setOpenDrawer] = useState(false);
-  const [paramsState, setParamsState] = useState({ sortBy: '', view: '', gender: [], category: [] });
-  const router = useRouter();
-  const pathname = usePathname();
   const searchParams = useSearchParams();
-
-  const toggleDrawer = (newOpen) => {
-    setOpenDrawer(newOpen);
-  };
+  const router = useRouter();
+  const [openDrawer, setOpenDrawer] = useState(false);
+  const [paramsState, setParamsState] = useState({
+    sortBy: '',
+    view: '',
+    minPrice: '',
+    maxPrice: '',
+    gender: [],
+    category: [],
+  });
 
   useEffect(() => {
-    const keys = ['sortBy', 'view', 'gender', 'category'];
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    toggleDrawer(false);
     const newState = {};
-    keys.forEach((key) => {
+    Object.keys(paramsState).forEach((key) => {
       if (key === 'gender' || key === 'category') {
         const items = searchParams.get(key);
         const arrItems = items ? items.split(',') : [];
@@ -45,147 +36,142 @@ export default function FragrancePage() {
     setParamsState(newState);
   }, [searchParams]);
 
-  // console.log(paramsState);
+  const toggleDrawer = (newOpen) => {
+    setOpenDrawer(newOpen);
+  };
 
-  const makeRout = (obj) => {
+  const applyFilters = (obj) => {
     const params = new URLSearchParams(searchParams.toString());
     Object.keys(obj).forEach((key) => {
-      if (key !== 'sortBy' && key !== 'view') {
+      if (key !== 'sortBy' && key !== 'view' && key !== 'minPrice' && key !== 'maxPrice') {
         params.set(key, obj[key].join(','));
       } else {
         params.set(key, obj[key]);
       }
     });
 
-    router.push(`?${params.toString()}`, undefined, { scroll: true });
+    router.push(`?${params.toString()}`, undefined, { scroll: false });
+    toggleDrawer(false);
   };
 
-  const handleChangeCategoryParams = (value, routTo) => {
-    let categories = [];
-    if (paramsState.category.includes(value)) {
-      categories = paramsState.category.filter((f) => f !== value);
+  const makeRout = (prop, value) => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set(prop, value);
+    router.push(`?${params.toString()}`, undefined, { scroll: false });
+  };
+
+  const makeArrayRoute = (prop, arr) => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set(prop, arr.join(','));
+    router.push(`?${params.toString()}`, undefined, { scroll: false });
+  };
+
+  const handleChangeArrayParams = (prop, value, rout) => {
+    let items = [];
+    if (paramsState[prop].includes(value)) {
+      items = paramsState[prop].filter((f) => f !== value);
     } else {
-      categories = [...paramsState.category, value];
+      items = [...paramsState[prop], value];
     }
-    const newState = { ...paramsState, category: categories };
+    const newState = { ...paramsState, [prop]: items };
     setParamsState(newState);
-    if (routTo) {
-      makeRout(newState);
+    if (rout) {
+      makeArrayRoute(prop, items);
     }
   };
 
-  const handleChangeParams = (key, value, routTo) => {
-    if (key === 'gender') {
-      let genderArr = [];
-      if (paramsState[key].includes(value)) {
-        genderArr = paramsState[key].filter((f) => f !== value);
-      } else {
-        genderArr = [...paramsState[key], value];
-      }
-
-      const newState = { ...paramsState, [key]: genderArr };
-      setParamsState(newState);
-
-      if (routTo) {
-        makeRout(newState);
-      }
-      // }
-    } else {
-      const newState = { ...paramsState, [key]: value };
-      setParamsState(newState);
-      if (routTo) {
-        makeRout(newState);
-      }
+  const handleChangeParams = (prop, value, rout) => {
+    setParamsState({ ...paramsState, [prop]: value });
+    if (rout) {
+      makeRout(prop, value);
     }
   };
 
-  // console.log(paramsState);
-
-  return (
-    <Grid sx={{ m: { xs: '50px 15px', sm: '90px 35px' } }} size={12}>
-      <Box
-        sx={{
-          display: 'flex',
-          maxWidth: '1200px',
-          margin: '0 auto', // center horizontally
-          flexWrap: 'wrap',
-        }}
-      >
-        <Box sx={{ display: 'flex', width: '100%', alignItems: 'flex-start', flexWrap: 'wrap' }}>
-          <Typography
-            sx={{
-              fontSize: { xs: '18px', sm: '30px' },
-              fontWeight: { xs: 500, sm: 600 },
-              flexGrow: 1,
-              lineHeight: { xs: '20px', sm: '30px' },
-              mb: '20px',
-            }}
-          >
-            Fragrance
-          </Typography>
-          <SortView
-            toggleDrawer={toggleDrawer}
-            handleChangeParams={handleChangeParams}
-            paramsState={paramsState}
-          />
-        </Box>
-
-        <Box sx={{ display: { xs: 'none', sm: 'none', md: 'block' }, mt: '40px' }}>
-          <Filter
-            handleChangeCategoryParams={handleChangeCategoryParams}
-            handleChangeParams={handleChangeParams}
-            paramsState={paramsState}
-          />
-        </Box>
-
-        <Drawer
-          sx={{ '& .MuiDrawer-paper': { width: '100%' } }}
-          open={openDrawer}
-          onClose={() => toggleDrawer(false)}
+    //   console.log(paramsState);
+    return (
+      <Grid sx={{ m: { xs: '50px 15px', sm: '90px 35px' } }} size={12}>
+        <Box
+          sx={{
+            display: 'flex',
+            maxWidth: '1200px',
+            margin: '0 auto', // center horizontally
+            flexWrap: 'wrap',
+          }}
         >
-          <div>
-            <Box
+          <Box sx={{ display: 'flex', width: '100%', alignItems: 'flex-start', flexWrap: 'wrap' }}>
+            <Typography
               sx={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                position: 'sticky',
-                top: 0,
-                bgcolor: 'white',
-                boxShadow: 'rgba(0, 0, 0, 0.1) 0px 2px 6px',
-                p: ' 10px 15px',
-                zIndex: 100,
-                alignItems: 'center',
+                fontSize: { xs: '18px', sm: '30px' },
+                fontWeight: { xs: 500, sm: 600 },
+                flexGrow: 1,
+                lineHeight: { xs: '20px', sm: '30px' },
+                mb: '20px',
               }}
             >
-              <CloseIcon sx={{ color: '#8a8c8dff' }} onClick={() => toggleDrawer(false)} />
-              <Button
-                onClick={() => {
-                  toggleDrawer(false);
-                  makeRout(paramsState);
+              Fragrance
+            </Typography>
+            <SortView
+              toggleDrawer={toggleDrawer}
+              handleChangeParams={handleChangeParams}
+              paramsState={paramsState}
+            />
+          </Box>
+          <Box sx={{ display: { xs: 'none', sm: 'none', md: 'block' }, mt: '40px' }}>
+            <Filter
+              paramsState={paramsState}
+              handleChangeParams={handleChangeParams}
+              makeRout={true}
+              handleChangeArrayParams={handleChangeArrayParams}
+            />
+          </Box>
+          <Drawer
+            sx={{ '& .MuiDrawer-paper': { width: '100%' } }}
+            open={openDrawer}
+            onClose={() => toggleDrawer(false)}
+          >
+            <div>
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  position: 'sticky',
+                  top: 0,
+                  bgcolor: 'white',
+                  boxShadow: 'rgba(0, 0, 0, 0.1) 0px 2px 6px',
+                  p: ' 10px 15px',
+                  zIndex: 100,
+                  alignItems: 'center',
                 }}
-                variant="text"
-                sx={{ m: 0 }}
               >
-                Aply
-              </Button>
-            </Box>
-            <div style={{ padding: '15px' }}>
-              <Filter
-                handleChangeCategoryParams={handleChangeCategoryParams}
-                handleChangeParams={handleChangeParams}
-                paramsState={paramsState}
-                makeRout={makeRout}
-              />
-              <Filter
-                handleChangeCategoryParams={handleChangeCategoryParams}
-                handleChangeParams={handleChangeParams}
-                paramsState={paramsState}
-                makeRout={makeRout}
-              />
+                <CloseIcon sx={{ color: '#8a8c8dff' }} onClick={() => toggleDrawer(false)} />
+                <Button
+                  onClick={() => {
+                    toggleDrawer(false);
+                    applyFilters(paramsState);
+                  }}
+                  variant="text"
+                  sx={{ m: 0 }}
+                >
+                  Aply
+                </Button>
+              </Box>
+              <div style={{ padding: '15px' }}>
+                <Filter
+                  paramsState={paramsState}
+                  handleChangeParams={handleChangeParams}
+                  makeRout={false}
+                  handleChangeArrayParams={handleChangeArrayParams}
+                />
+                <Filter
+                  paramsState={paramsState}
+                  handleChangeParams={handleChangeParams}
+                  makeRout={false}
+                  handleChangeArrayParams={handleChangeArrayParams}
+                />
+              </div>
             </div>
-          </div>
-        </Drawer>
-      </Box>
-    </Grid>
-  );
+          </Drawer>
+        </Box>
+      </Grid>
+    );
 }
