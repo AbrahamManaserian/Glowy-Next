e894dbf9783ae8faadb67b2cfefe83f18d5a0dac
AbrahamManaserian <abraham020788@gmail.com>
2025-09-26 22:58:06 +0400

    ...

--- src/app/fragrance/[product]/loading.js
+++ src/app/fragrance/[product]/loading.js
@@ -1,11 +1,5 @@
 import { Box, LinearProgress } from '@mui/material';
 
 export default function Loading() {
-  return (
-    <div style={{ minHeight: '30vh' }}>
-      <Box sx={{ width: '100%' }}>
-        <LinearProgress />
-      </Box>
-    </div>
-  );
+  return null;
 }
