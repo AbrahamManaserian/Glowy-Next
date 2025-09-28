bbcc3d9b69171e8f45725a628e818fbed0706727
AbrahamManaserian <abraham020788@gmail.com>
2025-09-28 17:11:12 +0400

    ...

--- src/components/ScrollToTop.js
+++ src/components/ScrollToTop.js
@@ -3,11 +3,22 @@ import { usePathname } from 'next/navigation';
 import { useEffect } from 'react';
 
 export default function ScrollToTop() {
-  const pathname = usePathname();
-
+  // const pathname = usePathname();
   useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+    // Disable browser's built-in scroll restoration
+    if ('scrollRestoration' in window.history) {
+      window.history.scrollRestoration = 'manual';
+    }
+  }, []);
 
   return null;
 }
+// export default function ScrollToTop() {
+//   const pathname = usePathname();
+
+//   useEffect(() => {
+//     window.scrollTo(0, 0);
+//   }, [pathname]);
+
+//   return null;
+// }
