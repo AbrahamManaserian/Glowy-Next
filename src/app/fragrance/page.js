973efa6b1a9184cd13308ed400bfe75764e4bd74
AbrahamManaserian <abraham020788@gmail.com>
2025-12-07 14:22:48 +0400

    ...

--- src/app/fragrance/page.js
+++ src/app/fragrance/page.js
@@ -10,9 +10,9 @@ export default async function FragrancePage({ searchParams }) {
     process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://glowy-store-next.netlify.app';
 
   const res = await fetch(`${baseUrl}/api/fragrance?${queryString}`, {
-    cache: 'no-store', // avoids caching issues
+    // cache: 'no-store', // avoids caching issues
     // cache: 'force-cache', // default
-    // next: { revalidate: 360 },
+    next: { revalidate: 360 },
   });
 
   const data = await res.json();
